5534e649e992f6fefe2188fce5eb7d1fa1b7f1a3
Yunchia Hsu <127295359+Yunchia-Hsu@users.noreply.github.com>
2024-05-27 17:52:13 +0300

    fixed: libft:ft_putstr_fd

--- libft/ft_putstr_fd.c
+++ libft/ft_putstr_fd.c
@@ -3,23 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alli <alli@student.hive.fi>                +#+  +:+       +#+        */
+/*   By: yhsu <yhsu@hive.student.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 14:06:48 by alli              #+#    #+#             */
-/*   Updated: 2023/11/13 11:41:51 by alli             ###   ########.fr       */
+/*   Updated: 2024/05/27 17:48:34 by yhsu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putstr_fd(char *s, int fd)
+int	ft_putstr_fd(char *s, int fd)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
-	while (s[i] != '\0')
+	if (! s)
 	{
-		write(fd, &s[i], 1);
-		i++;
+		if (ft_putstr_fd("(null)", fd) == -1)
+			return (-1);
+		return (0);
 	}
+	if (write (fd, s, ft_strlen(s)) == -1)
+		return (-1);
+	return (0);
 }
