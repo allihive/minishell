6e0ae9a5b5d4df2eda423e53b5eb8ef6c489d0a7
Alice Li <alli@c1r2p3.hive.fi>
2024-06-26 09:57:19 +0300

    expand_the_shit is workinggit branchgit branch

--- srcs/expand.c
+++ srcs/expand.c
@@ -6,7 +6,7 @@
 /*   By: alli <alli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 18:37:09 by yhsu              #+#    #+#             */
-/*   Updated: 2024/06/25 16:57:12 by alli             ###   ########.fr       */
+/*   Updated: 2024/06/26 09:55:12 by alli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,29 @@
 // 	return (result);
 // }
 
+static int	key_exists(t_shell *ms, char *name)
+{
+	int		len;
+	int		i;
+	char	*key;
+
+	i = 0;
+	while(name[i] && name[i] != '=')
+		i++;
+	key = ft_substr(name, 0, i + 1);
+	if (!key)
+		return (0); //should be error_handle
+	len = ft_strlen(key + 1);
+	i = 0;
+	while (i < ms->envp_size && ms->envp[i])
+	{
+		if ((ft_strncmp(key, ms->envp[i], len) == 0) 
+			&& (ms->envp[i][len] == '\0' || ms->envp[i][len] == '='))
+				return (1);// key is found
+		i++;
+	}
+	return (0);
+}
 
 
 char *shrink(char *cmd, int remove) //if something after $ is invalid still print something before and after
@@ -44,12 +67,10 @@ char *shrink(char *cmd, int remove) //if something after $ is invalid still prin
 	char *temp;
 	char *new;
 	
-	if (cmd[remove] == '\0')
-		return (NULL);
-	printf("cmd[remove]: %c\n", cmd[remove]);
-	printf("cmd %s\n", cmd);
-	printf("strlen of cmd: %ld\n", ft_strlen(cmd));
-	printf("remove %d\n", remove);
+	// printf("cmd[remove]: %c\n", cmd[remove]);
+	// printf("cmd %s\n", cmd);
+	// printf("strlen of cmd: %ld\n", ft_strlen(cmd));
+	// printf("remove %d\n", remove);
 	new = ft_calloc(ft_strlen(cmd), sizeof(char)); //cannot be expanded, the delete the dollar sign
 	if (!new)
 		return (NULL);
@@ -59,10 +80,13 @@ char *shrink(char *cmd, int remove) //if something after $ is invalid still prin
 		i++;
 	i++;
 	new[j+1] = ' ';
-	while(cmd[i])
+	while(cmd[i - 1])
 		new[j++] = cmd[i++];
+	dprintf(2, "new: %s\n", new);
 	temp = cmd;
-	cmd = new; 
+	// free (cmd);
+	cmd = new;
+	dprintf(2, "new cmd: %s\n", cmd);
 	free (temp);
 	return (cmd);
 	//echo hello $USEroijg haha prints hello haha
@@ -113,73 +137,60 @@ char *get_value(int start, int len , char *cmd, t_shell *ms)
 	int i;
 	char *value;
 	int value_start;
-	// int	j;
-	
-	// j = 0;
-	dprintf(2,"leng: %d\n", len);
-	dprintf(2, "cmd in get value: %s\n", cmd);
+
+	// dprintf(2, "cmd in get value: %s\n", cmd);
 	value_start = start;//after quotes and dollar signs
 	key = ft_calloc(len + 1, sizeof(char));
 	if (!key)
 		return (NULL);
-	;
 	i = 0;
 	while (i < len)
 		key[i++]= cmd[start++];// copy key words from cmd to key
-	;
 	i = 0;
-	dprintf(2,"key: %s\n", key);
-	dprintf(2, "keysize: %ld\n", ft_strlen(key));
-	dprintf(2, "keysize: %ld\n", sizeof(key));
-	dprintf(2, "envp_size %d\n", ms->envp_size);
-	// while (ms->envp[i])
-	// {
-	// 	if (ft_strncmp(key, ms->envp[i], len))
-	// 	{
-	// 		dprintf(2, "ms->envp[%d]:%s\n", i, ms->envp[i]);
-	// 		dprintf(2, "ft_strncmp %d\n", ft_strncmp(key, ms->envp[i], len));
-	// 		i++;
-	// 	}
-	// 	else //(ms->envp[i + 1] == NULL)
-	// 	{
-	// 		dprintf(2, "entered ms->envp[i+ 1]");
-	// 		break ;
-	// 	}
-	// 	// else
-	// 	// 	i++;
-	// }
-	printf("envp_size: %d", ms->envp_size);
-	while (i < ms->envp_size)
+	// dprintf(2,"key: %s\n", key);
+	// dprintf(2, "keysize: %ld\n", ft_strlen(key));
+	// dprintf(2, "envp_size before %d\n", ms->envp_size);
+	while (i < ms->envp_size) //iterates through the whole list
 	{
-		if (ft_strncmp(key, ms->envp[i], len))
+		if (!key_exists(ms, key)) //doesn't match, then it will iterate through the list
 		{
-			dprintf(2, "ms->envp[%d]:%s\n", i, ms->envp[i]);
-			dprintf(2, "ft_strncmp %d\n", ft_strncmp(key, ms->envp[i], len));
+			// dprintf(2, "ms->envp[%d]:%s\n", i, ms->envp[i]);
+			// dprintf(2, "ft_strncmp %d\n", ft_strncmp(key, ms->envp[i], len));
 			i++;
 		}
-		else if (ft_strncmp(key, ms->envp[i], len) == 0)
+		else if (key_exists(ms, key)) //breaks if it equals or is at the end of list
 			break ;
-		// else if()
 	}
-	// dprintf(2, "ms->envp[i+ 1] %s\n", ms->envp[i + 1]);
-	if (i == ms->envp_size)
+	dprintf(2, "envp_size after %d\n", ms->envp_size);
+	dprintf(2, "i: %d\n", i);
+	if (i == ms->envp_size) // when it's at the end of the list we should go into the shrink function
 	{
 			// dprintf(2, "test4\n");
-		dprintf(2, "cmd: %s\n", cmd);
-		dprintf(2,"start: %d\n", start);
-		dprintf(2,"cmd[value_start - 1]: %c\n", cmd[value_start - 1]);
-		if (!shrink(cmd, value_start - 1))//something which is not valid if it's a string, then the start count is correct. else the start is wrong because different arguments
-			return (NULL);
-	}	
-	if (ft_strncmp(key, ms->envp[i], len) == 0) //use name_exists here?
+		// dprintf(2, "cmd: %s\n", cmd);
+		// dprintf(2,"value_start: %d\n", value_start);
+		// dprintf(2,"cmd[value_start - 1]: %c\n", cmd[value_start - 1]);
+		// if (!(shrink(cmd, value_start - 1)))//something which is not valid if it's a string, then the start count is correct. 
+		// {
+		// 	dprintf(2, "entered !shrink");
+		// 	return (NULL);
+		// }
+		// else
+		// {
+			// dprintf(2,"cmd before shrink %s\n", cmd);
+			cmd = shrink(cmd, value_start - 1);
+			if (!cmd)
+				return (NULL); //error_ handle
+			// dprintf(2, "cmd after shrink %s\n", cmd);
+		// }
+	}
+	if (key_exists(ms, key))
 	{
 			dprintf(2, "entered strncmp: %s\n", cmd);
 			value = (ft_strchr(ms->envp[i], '=') + 1);// USER=yhsu    value = yhsu
-			cmd = add_value_back(value, value_start, len, cmd);//need to be protected //replace the key putting the value in there
+			cmd = add_value_back(value, value_start, len, cmd);//need to be protected //changed start to value start
 			dprintf(2, "cmd after add_value_back: %s\n", cmd);
-			// break;; //keep the single quote inside, double quote inside, don't need to expand, then keep double quote inside
+			 //keep the single quote inside, double quote inside, don't need to expand, then keep double quote inside
 	}
-
 	free(key);
 	dprintf(2, "cmd in get_value:%s\n", cmd);	
 	return (cmd);	
@@ -232,13 +243,13 @@ char *expand_the_shit_out(char *cmd, t_process_node *mod, t_shell *ms)//send the
 			printf("i: %d\n", i);
 			printf("start: %d\n", start);
 			result = get_value(start, i - start, result , ms); // need error handling		
-			dprintf(2,"result:%s\n", result);
-			continue;
+			dprintf(2,"in expand_the_shit_out result:%s\n", result);
+			// continue;
+			break ;
 		}
 		else
 			i++;
 	}
-
 	dprintf(2,"at the end result:%s\n", result);
 	//delete quote function
 	return (result);
