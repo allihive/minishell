f30385b6c56a3829152ec27c4854d70b6697ab6b
Yun-Chia Hsu <yh24hsu@gmail.com>
2024-07-16 23:45:46 +0300

    fixed:milti heredocs

--- srcs/heredoc.c
+++ srcs/heredoc.c
@@ -20,10 +20,9 @@ void get_heredoc_input(int heredoc_fd, t_process_node *process)
 
     line = get_next_line(STDIN_FILENO);
     delimiter = (ft_strjoin( process->here_doc, "\n"));
-    // delimiter = process->here_doc;
-    // dprintf(2, "get_heredoc_input line:%s\n", line);
     dprintf(2, "delimiter:%s\n", delimiter);
     while (line && ft_strncmp(line, delimiter, ft_strlen(delimiter) - 2) != 0 )// need to fix this my delimiter has invisible char (ascii 22)
+    //while (line && ft_strncmp(line, delimiter, ft_strlen(delimiter)) != 0 )
     {
         dprintf(2, "get_heredoc_input line:%s\n", line);
         dprintf(2, "get_heredoc_input delimiter:%s\n", delimiter);
@@ -39,11 +38,6 @@ void get_heredoc_input(int heredoc_fd, t_process_node *process)
         printf("line=%s(%zu), delimiter=%s(%zu)\n", line, ft_strlen(line), delimiter, ft_strlen(delimiter) - 2);
         printf("%d\n", ft_strncmp(line, delimiter, 3));
     }
-    
-    // dprintf(2,"process->heredoc:%s\n", process->here_doc);
-    dprintf(2,"process->heredoc:%s\n", process->here_doc);
-
-
     // while (1)//fix should change to g_signal != 1
     // {
     //     line = readline("> ");
@@ -61,7 +55,7 @@ void get_heredoc_input(int heredoc_fd, t_process_node *process)
     //dprintf(2, "process->here_doc:%s\n", process->here_doc);
 }
 
-char *right_delimiter(char *redirect,  t_process_node *process)
+int right_delimiter(char *redirect,  t_process_node *process)// too many lines can remove tmp just use redirect 
 {
     char *end;
     char *tmp = redirect;
@@ -72,25 +66,27 @@ char *right_delimiter(char *redirect,  t_process_node *process)
     tmp = redirect;
     while (*end && !ifisredirect(*end))
 		end++;
-
     if (process->here_doc == NULL) 
     {
-        process->here_doc = malloc(sizeof(char *) * 100); // Define MAX_REDIRECTS appropriately
+        //process->here_doc = malloc(sizeof(char *) * 100); // Define MAX_REDIRECTS appropriately
+        process->here_doc = calloc(100, sizeof(char *));
         if (process->here_doc== NULL) 
         {
             perror("heredoc in malloc");
-            return NULL; // Handle error or return as appropriate
+            return (1); // Handle error or return as appropriate
         }
-        ft_memset(process->here_doc, 0, sizeof(char *) * 100); // Initialize to NULL
+        //ft_memset(process->here_doc, 0, sizeof(char *) * 100); // Initialize to NULL
     }
     tmp = ft_substr(redirect, 0, end - tmp);
-    //dprintf(2, "tmp in right limiter: %s\n", tmp);
-    //dprintf(2, "end in right limiter: %s\n", end); 
-    
+    if (!tmp) 
+    {
+        perror("heredoc in malloc");
+        return (1); // Handle error or return as appropriate
+    }
     if (*tmp == '"' || *tmp == '\'')
         tmp = remove_quote(tmp, ft_strlen(tmp) - 1);
-    dprintf(2, "redirect in right limiter: %s\n", tmp);
-    return (tmp);
+    process->here_doc = tmp;
+    return (0);
 }
 
 int handle_heredocs(char *redirect, t_process_node *process,t_shell *ms)
@@ -105,21 +101,23 @@ int handle_heredocs(char *redirect, t_process_node *process,t_shell *ms)
     i++;
     heredoc_name = ft_strjoin(".heredoc", ft_itoa(i));
     redirect+= 2;
-    process->here_doc = right_delimiter(redirect, process);//delimiter: end
-
+    //process->here_doc = right_delimiter(redirect, process);//delimiter: end
+    if (right_delimiter(redirect, process))
+     {
+        perror("open .heredoc failed");
+        return(set_exitcode(ms, 1)); 
+    }   
     close(ms->fd[0]);
     heredoc_fd = open(heredoc_name, O_CREAT | O_RDWR | O_TRUNC, 0777);
     
     if (heredoc_fd == -1)
     {
-   
         perror("open .heredoc failed");
         return(set_exitcode(ms, 1)); 
     }
     get_heredoc_input(heredoc_fd, process);
     if (close(heredoc_fd) == -1)
     {
-    
         perror("open .heredoc failed");
         return(set_exitcode(ms, 1)); 
     }
