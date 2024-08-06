ffab1ee279824b2ef682314cf19181efe8e01440
Alice Li <alli@c2r5p8.hive.fi>
2024-08-06 10:15:33 +0300

    ghost pwd

--- srcs/builtin/pwd.c
+++ srcs/builtin/pwd.c
@@ -6,39 +6,39 @@
 /*   By: alli <alli@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 09:25:55 by alli              #+#    #+#             */
-/*   Updated: 2024/08/01 09:56:31 by alli             ###   ########.fr       */
+/*   Updated: 2024/08/06 08:55:50 by alli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-void	pwd(t_shell *ms, char **cmd, int fd) //will be a child built in
-{
-	(void)cmd;
-	char	*pwd_path;
-	pwd_path = getcwd(NULL, 0);
-	
-	if (!pwd_path)
-		error_handle(ms);
-	ms->cwd = pwd_path;
-	ft_putstr_fd(ms->cwd, fd);
-	ft_putchar_fd('\n', fd);
-	ms->excode = 0;
-	free(pwd_path);
-}
-// void	pwd(t_shell *ms, char **cmd, int fd) // which one?
+// void	pwd(t_shell *ms, char **cmd, int fd) //will be a child built in
 // {
-// 	char	*str;
-// 	char	buf[1000];
-	
 // 	(void)cmd;
-
-// 	str = getcwd(buf, 1000);
-// 	if (str)
-// 	{
-// 		ft_putendl_fd(str, fd);
-// 		ms->excode = 0;
-// 	}
-// 	else
-// 		ft_putendl_fd(ms->cwd, fd);
+// 	char	*pwd_path;
+// 	pwd_path = getcwd(NULL, 0);
+	
+// 	if (!pwd_path)
+// 		error_handle(ms);
+// 	ms->cwd = pwd_path;
+// 	ft_putstr_fd(ms->cwd, fd);
+// 	ft_putchar_fd('\n', fd);
+// 	ms->excode = 0;
+// 	free(pwd_path);
 // }
+void	pwd(t_shell *ms, char **cmd, int fd) // which one?
+{
+	char	*str;
+	char	buf[1000];
+	
+	(void)cmd;
+
+	str = getcwd(buf, 1000);
+	if (str)
+	{
+		ft_putendl_fd(str, fd);
+		ms->excode = 0;
+	}
+	else
+		ft_putendl_fd(ms->cwd, fd);
+}
